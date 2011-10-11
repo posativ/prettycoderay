5c26db0ee10ca9ffca7b24bc17318131600bfc13
posativ <info@posativ.org>
2011-10-11 20:39:21 +0200

    test for required css class

--- gen_pygments.sh
+++ gen_pygments.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "please provide a css class"
+    exit 1
+fi
+
 for VAR in `python -c "from pygments.styles import get_all_styles; print '\n'.join(get_all_styles())"`; do
     pygmentize -S $VAR -f html | python convert.py --class $1 > css/$VAR.css
 done
